50bcc1d485c5863227b3ada2f9f9af4027280347
F1reN1kto <maksimcymbal77@gmail.com>
2025-02-14 17:29:48 +0700

    Доделал login по методичке

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createStore } from 'vuex'
+import { loginRequest } from "@/utils/api";
 
-const routes = [
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+export default createStore({
+  state: {
+    token: localStorage.getItem('myAppToken') || '',
+  },
+  getters: {
+    isAuthenticated: state => {
+    }
+  },
+  actions: {
+    AUTH_REQUEST({commit}, user) {
+      return new Promise((resolve, reject) => {
+        loginRequest(user)
+            .then(response => {
+              commit('AUTH_REQUEST', response);
+              localStorage.setItem('myAppToken', token);
+              resolve()
+            })
+            .catch(error => {
+              commit('AUTH_REQUEST', error)
+              localStorage.removeItem('myAppToken');
+              reject
+            })
+      })
+    }
+  },
+  mutations: {
+    AUTH_REQUEST(state, token) {
+      state.token = token;
+    },
+    AUTH_ERROR(state) {
+      state.token = '';
+    }
+  }
 })
-
-export default router
